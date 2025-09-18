d7855c241d8faeb2d051bfe68c532ebbf6cf0300
noppanatt <noppanat.b@codemonday.com>
2025-09-19 03:11:58 +0700

    fix vercel json

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,5 @@
-import * as tsconfigPaths from "tsconfig-paths";
+import "tsconfig-paths/register";
 import app from "../src/index";
 
-tsconfigPaths.register({
-  baseUrl: process.cwd(),
-  paths: {
-    "@routes/*": ["src/routes/*"],
-    "@utils/*": ["src/utils/*"],
-    "@constants/*": ["src/constants/*"],
-    "@dto/*": ["src/dto/*"],
-    "@validation/*": ["src/middlewares/validation/*"],
-    "@database/*": ["src/database/*"],
-    ".commit-id": ["commit-id"],
-  },
-});
-
+// export the app (no app.listen here)
 export default app;
